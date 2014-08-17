493aba2f7f1931131ff39cafe62a62b0c27ee146
Shao <shaohan.chen@berkeley.edu>
2014-08-16 18:25:05 -0700

    [Added] working tabs

--- shirtsio/static/js/app.js
+++ shirtsio/static/js/app.js
@@ -1,1 +1,21 @@
 /* app.js */
+
+/* for working-tab */
+jQuery(document).ready(function() {
+    jQuery('.tabs .tab-links a').on('click', function(e)  {
+        var currentAttrValue = jQuery(this).attr('href');
+
+        // Show/Hide Tabs
+        jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+
+        // Change/remove current tab to active
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
+        // Show/Hide Tabs
+        jQuery('.tabs ' + currentAttrValue).fadeIn(400).siblings().hide();
+
+        e.preventDefault();
+    });
+
+
+});
